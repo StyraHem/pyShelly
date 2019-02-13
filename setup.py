1bc9cdbb90a27efe508916cb1d5f05d95cd59187
Håkan Åkerberg <hakan@tarra.se>
2019-02-13 21:16:02 +0000

    Prepare for PyPi rel 0.0.2

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyShelly-styrahem",
-    version="0.0.1",
-    author="Håkan Åkerberg",
-    author_email="hakan@tarra.se",
-    description="A Python library for Shelly smart home devices",
+    name="pyShelly",
+    version="0.0.2",
+    author="StyraHem / Tarra AB",
+    author_email="info@styrahem.se",
+    description="Library for Shelly smart home devices",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/StyraHem/pyShelly",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
