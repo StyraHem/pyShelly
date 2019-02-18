702ef6fb473b5d809ecadad9d8415d7cce371af1
Håkan Åkerberg <hakan@tarra.se>
2019-02-18 21:28:13 +0000

    Change version handling in setup

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 import setuptools
+import re
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def read(*parts):
+    with open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 setuptools.setup(
     name="pyShelly",
-    version="0.0.6",
+    version=find_version("pyShelly","__init__.py"),
     author="StyraHem / Tarra AB",
     author_email="info@styrahem.se",
     description="Library for Shelly smart home devices",
