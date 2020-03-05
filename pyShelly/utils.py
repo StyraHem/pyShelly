f0afbf23a58dd853cae45d8b9b916c63188003e6
Colin Frei <colin@colinfrei.com>
2020-03-05 14:54:56 +0100

    Add method to POST
    It'd be nice to update settings, adding this method here makes it easier to use it in https://github.com/StyraHem/ShellyForHASS

--- pyShelly/utils.py
+++ pyShelly/utils.py
@@ -4,6 +4,7 @@
 import base64
 import traceback
 import json
+import urllib
 from datetime import datetime
 from .compat import s
 
@@ -74,6 +75,48 @@ def shelly_http_get(host, url, username, password, log_error=True):
         else:
             LOGGER.debug(
                 "Fail http GET: %s %s %s", host, url, ex)
+    finally:
+        if conn:
+            conn.close()
+
+    return success, res
+
+def shelly_http_post(host, url, body, username, password, log_error=True):
+    """Send HTTP POST request"""
+    res = ""
+    success = False
+    conn = None
+    try:
+        LOGGER.debug("http://%s%s", host, url)
+        conn = httplib.HTTPConnection(host, timeout=5)
+        headers = {}
+        if username is not None \
+            and password is not None:
+            combo = '%s:%s' % (username, password)
+            auth = s(
+                base64.b64encode(combo.encode()))  # .replace('\n', '')
+            headers["Authorization"] = "Basic %s" % auth
+        conn.request("POST", url, urllib.parse.urlencode(body), headers)
+        resp = conn.getresponse()
+
+        if resp.status == 200:
+            body = resp.read()
+            #LOGGER.debug("Body: %s", body)
+            res = json.loads(s(body))
+            success = True
+            LOGGER.debug("http://%s%s - Ok", host, url)
+        else:
+            res = "Error, " + str(resp.status) \
+                            + ' ' + str(resp.reason)
+            LOGGER.warning(res)
+    except Exception as ex:
+        success = False
+        res = str(ex)
+        if log_error:
+            exception_log(ex, "Error http POST: http://{}{}", host, url)
+        else:
+            LOGGER.debug(
+                "Fail http POST: %s %s %s", host, url, ex)
     finally:
         if conn:
             conn.close()
