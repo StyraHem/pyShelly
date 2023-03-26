8b5464461255ce5c049977aaf5eb9bbcbfc01f22
Philipp von dem Bussche <philipp.bussche@cloudreach.com>
2023-03-26 12:50:37 +0200

    Adding some more explanations to my test script.

--- test.py
+++ test.py
@@ -7,13 +7,15 @@ def device_added(dev,code):
 shelly = pyShelly()
 shelly.update_status_interval = timedelta(seconds=300)
 
-# enable sync with data in cloud (e.g. room name) by configuring cloud settings
+# enable sync with data in cloud (e.g. to get the device's room name) by configuring cloud settings
 # shelly.set_cloud_settings(os.environ.get('SHELLY_HOST'), os.environ.get('SHELLY_AUTH_KEY'))
 
 shelly.prometheus_enabled = True
 
 shelly.cb_device_added.append(device_added)
 shelly.start()
+# the below could be used to add a Shelly Plug S which is running in factory mode 
+# alternatively devices could be discovered (CoAP) using shelly.discover()
 shelly.add_device_by_ip("192.168.33.1", "IP-addr")
 
 while True:
